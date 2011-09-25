a685cf55a45dcacc082219084c630971c2f3c7a0
Yin Wang <yinwang0@gmail.com>
2011-09-25 18:46:43 -0400

    trivial changes.

--- conv1.hs
+++ conv1.hs
@@ -1,6 +1,6 @@
 -- Type checker for dependent types from Thierry Coquand:
 -- http://www.cse.chalmers.se/~coquand/conv1.hs
--- reformatted and added some comments
+-- reformatted, simplified and added some comments
 
 
 type Id = String
@@ -23,10 +23,8 @@ type FVal = (Id, Exp, Env)
 type Env  = [(Id, Val)]
 
 
-
 update :: Env -> Id -> Val -> Env
 update env x u = (x,u):env
-
 
 
 lookUp :: Id -> Env -> Val
@@ -37,18 +35,15 @@ lookUp x [] = error ("unbound variable: " ++ x)
 
 
 
--- the weak head normal form (whnf) algorithm
+-- Weak Head Normal Form (WHNF) algorithm
 
 fapp :: FVal -> Val -> Val
 fapp (x, e, env) u = eval (update env x u) e
 
 
-
 app :: Val -> Val -> Val
 app (VAbs f) x = fapp f x     -- can reduce with beta-reduction
-app v x = VApp v x            -- cannot reduce, store it as value
-
-
+app v x = VApp v x            -- cannot reduce, store as value
 
 
 eval :: Env -> Exp -> Val
@@ -58,15 +53,13 @@ eval env e =
      App e1 e2     -> app (eval env e1) (eval env e2)
      Type          -> VType
      Abs x e1      -> VAbs (x, e1, env)
-     Pi x a b      -> VPi (eval env a) (x, b, env) -- evaluate the type annotation
+     Pi x a b      -> VPi (eval env a) (x, b, env) -- evaluate annotation
 
 
 
-
--- the conversion algorithm; the integer is used to represent the
--- introduction of a fresh variable VGen == gensym. k is not used as a
--- state, because the check is recursive.
-
+-- The conversion algorithm (equality check). The integer is used to
+-- represent the introduction of a fresh variable VGen == gensym. k is
+-- not used as a state, because the comparison is strictly recursive.
 eqVal :: (Int, Val, Val) -> Bool
 eqVal (k, u1, u2) =
  case (u1, u2) of
@@ -83,20 +76,18 @@ eqVal (k, u1, u2) =
 
 
 
-
 ---------------  type-checking and type inference  --------------
-
 -- check an expression (e) against a type (v)
 checkExp :: (Int, Env, Env) -> Exp -> Val -> Bool
-checkExp (k, rho, gamma) e v =
- case (e, v) of
+checkExp (k, rho, gamma) e t =
+ case (e, t) of
    (Abs x e1, VPi w f) ->
-        let v = VGen k
-        in checkExp (k+1, update rho x v, update gamma x w) e1 (fapp f v)
+        let x' = VGen k
+        in checkExp (k+1, update rho x x', update gamma x w) e1 (fapp f x')
    (Pi x a b, VType) ->
         checkExp (k, rho, gamma) a VType &&
         checkExp (k+1, update rho x (VGen k), update gamma x (eval rho a)) b VType
-   _ -> eqVal (k, inferExp (k, rho, gamma) e, v)
+   _ -> eqVal (k, inferExp (k, rho, gamma) e, t)
 
 
 
@@ -106,12 +97,12 @@ inferExp (k, rho, gamma) e =
  case e of
    Var id -> lookUp id gamma
    App e1 e2 ->
-      case (inferExp (k, rho, gamma) e1) of
-        VPi w f ->
-          if checkExp (k, rho, gamma) e2 w
-               then fapp f (eval rho e2)
-          else error "wrong argument type"
-        _ -> error "trying to apply non-function"
+       case (inferExp (k, rho, gamma) e1) of
+         VPi w f ->
+             if checkExp (k, rho, gamma) e2 w
+             then fapp f (eval rho e2)
+             else error "wrong argument type"
+         _ -> error "trying to apply non-function"
    Type -> VType
    _ -> error "cannot infer type"
 
