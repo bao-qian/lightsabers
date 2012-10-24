4392578f16b6fb79cd0a8fa00aafe17706ebe738
Yin Wang <yinwang0@gmail.com>
2012-10-23 23:23:01 -0400

    what's going on with Git???

--- interp-lazy.rkt
+++ interp-lazy.rkt
@@ -1,59 +1,94 @@
-;; A call-by-need interpreter for lambda calculus with some primitives.
-;; Used with my SKI-compiler to demostrate the properties of my so-called
-;; lazy-SKI combinators.
-
-;; author: Yin Wang (yw21@cs.indiana.edu)
+;; A lazy interpreter for lambda calculus with some primitives
 
 
-(load "pmatch.scm")
+(define env0 '())
+
+(define ext
+  (lambda (x v env)
+    (cons (cons x v) env)))
+
+(define lookup
+  (lambda (x env)
+    (cond
+     [(assq x env) => cdr]
+     [else #f])))
+
+
+
+;; Thunk structure. Fields:
+;; 1. the function or the value depending on whether cached? is #t
+;; 2. whether the function has been forced already
+(struct thunk (fv cached?) #:transparent #:mutable)
+
+
+;; Closure structure
+(struct closure (fun env))
+
+
+
+;; Redefining "delay" and "force" of Scheme just for independence
+
+(define make-thunk
+  (lambda (fun)
+    (thunk fun #f)))
+
+(define force-thunk
+  (lambda (th)
+    (cond
+     [(thunk-cached? th)
+      (thunk-fv th)]
+     [else
+      (let loop ([val ((thunk-fv th))])
+        (cond
+         [(thunk? val)
+          (loop ((thunk-fv val)))]
+         [else
+          (set-thunk-fv! th val)
+          (set-thunk-cached?! th #t)
+          val]))])))
+
 
 
 (define interp1
   (lambda (exp env)
-    (pmatch exp
-      [,x (guard (symbol? x)) ((env x))]
-      [,x (guard (number? x)) x]
-      [(lambda (,x) ,e)
-       (lambda (v)
-         (interp1 e (lambda (x^)
-                      (if (eq? x^ x) v (env x^)))))]
-      [(,op ,e1 ,e2) (guard (memq op '(+ - * /)))
-       (let ([v1 (interp1 e1 env)]
-             [v2 (interp1 e2 env)])
-         (cond
-          [(and (number? v1) (number? v2))
-           ((eval op) v1 v2)]
-          [else
-           `(,op ,v1 ,v2)]))]
-      [(,f ,x) (guard (procedure? f))
-       (f (interp1 x env))]
-      [(,e1 ,e2)
-       (let ([v1 (interp1 e1 env)])
-         (cond
-          [(procedure? v1)
-           (v1 (let ([val? #f]
-                     [e (lambda () (interp1 e2 env))])
-                 (lambda ()
-                   (cond
-                    [val? e]
-                    [else
-                     (begin
-                       (set! val? #t)
-                       (set! e (e))
-                       e)]))))]
-          [else
-           `(,v1 ,(interp1 e2 env))]))]
-      [,exp (eval exp)])))
+    (match exp
+      [(? number? x) x]
+      [(? symbol? x)
+       (lookup x env)]
+      [`(lambda (,x) ,e)
+       (closure exp env)]
+      [`(,e1 ,e2)
+       (let ([v1 (make-thunk (lambda () (interp1 e1 env)))]
+             [v2 (make-thunk (lambda () (interp1 e2 env)))])
+         (make-thunk
+          (lambda ()
+            (let ([v1+ (force-thunk v1)])
+              (match v1+
+                [(closure `(lambda (,x) ,e) env1)
+                 (interp1 e (ext x v2 env1))])))))])))
 
 
 (define interp
   (lambda (exp)
-    (interp1 exp (lambda (x) (lambda () x)))))
+    (force-thunk (interp1 exp env0))))
+
 
 
 
 ;; ------------------------ tests -------------------------
+
 (interp
  '((lambda (x) 42)
    ((lambda (x) (x x))
     (lambda (x) (x x)))))
+
+(interp
+ '((lambda (x) 42)
+   ((lambda (x) x)
+    ((lambda (x) (x x))
+     (lambda (x) (x x))))))
+
+;;; infinite loop
+;; (interp
+;;  '((lambda (x) (x x))
+;;    (lambda (x) (x x))))
